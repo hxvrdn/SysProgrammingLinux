3100cb6e7028d6a104d1fef154059381b6c8ec2c
Harshavardhan Asode <hxvrdn@gmail.com>
2026-04-21 09:53:46 +0530

    First hello program from the course'

---
+++ chapter01/hello.c
@@ -0,0 +1,33 @@
+/*****************************************************************************
+  Title          : hello.c
+  Author         : Stewart Weiss
+  Created on     : December 11, 2022
+  Description    : A program that performs simple input and output
+  Purpose        : To introduce simple I/O
+  Usage          : hello
+  Build with     : gcc -o hello hello.c
+
+******************************************************************************
+* Copyright (C) 2025 - Stewart Weiss                                         *
+*                                                                            *
+* This code is free software; you can use, modify, and redistribute it       *
+* under the terms of the GNU General Public License as published by the      *
+* Free Software Foundation; either version 3 of the License, or (at your     *
+* option) any later version. This code is distributed WITHOUT ANY WARRANTY;  *
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+* PARTICULAR PURPOSE. See the file COPYING.gplv3 for details.                *
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char    name[256];
+    printf("Enter your name: ");
+    scanf("%255s", name);
+    printf("hello %s\n", name);
+    printf("The length of your name is %lu.\n", strlen(name));
+
+}
